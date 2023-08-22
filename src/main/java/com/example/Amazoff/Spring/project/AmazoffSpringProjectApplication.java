3596cfa592889b2f2677de3cafd3c1fa2dc55c48
Ashwini S Biradar <ashwinibiradar33@gmail.com>
2023-08-22 19:46:24 +0530

    done by ashwini

---
+++ src/main/java/com/example/Amazoff/Spring/project/AmazoffSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Amazoff.Spring.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazoffSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazoffSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Amazoff/Spring/project/AmazoffSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Amazoff.Spring.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazoffSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
